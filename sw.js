fa3c496c896e5d4ca300605cab54cb258e47daa0
KML2022 <99826858+KML2022@users.noreply.github.com>
2026-09-20 23:16:20 -0500

    Eliminar service worker

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="cdt-v4";
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/mis-cdt/","/mis-cdt/index.html"])));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
