c1f1a85793fb37eb9f3683e554236c8ae37d6dc2
KML2022 <99826858+KML2022@users.noreply.github.com>
2026-09-20 23:12:09 -0500

    SW v4 limpiar cache

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
-const CACHE="cdt-v3";
-const FILES=["/mis-cdt/","/mis-cdt/index.html"];
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
+const CACHE="cdt-v4";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/mis-cdt/","/mis-cdt/index.html"])));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
