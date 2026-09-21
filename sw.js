abfdce94422bb930d59c4fbce4e115f40f5bf002
KML2022 <99826858+KML2022@users.noreply.github.com>
2026-09-20 23:06:17 -0500

    Actualizar SW v3

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-const CACHE="cdt-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/mis-cdt/","/mis-cdt/index.html"]))));
+const CACHE="cdt-v3";
+const FILES=["/mis-cdt/","/mis-cdt/index.html"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
